940aabf26111f193584508146258ad347619dd03
waveofmymind <sjun0913@gmail.com>
2023-03-14 11:32:26 +0900

    [FEAT] increase 매핑 추가

---
+++ basic1/src/main/java/com/ll/basic1/HomeController.java
@@ -0,0 +1,37 @@
+package com.ll.basic1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    private static int a = 0;
+
+    @GetMapping("/home/main")
+    @ResponseBody
+    public String showMain() {
+        return "안녕하세요.";
+    }
+
+    @GetMapping("/home/main2")
+    @ResponseBody
+    public String showMain2() {
+        return "반갑습니다.";
+    }
+
+    @GetMapping("/home/main3")
+    @ResponseBody
+    public String showMain3() {
+        return "즐거웠습니다.";
+    }
+
+    @GetMapping("/home/increase")
+    @ResponseBody
+    public String increase() {
+
+        return String.valueOf(a++);
+
+    }
+}

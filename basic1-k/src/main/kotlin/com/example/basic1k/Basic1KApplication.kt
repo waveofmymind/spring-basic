7273e060ea79070b4ce72de2f11db658c8612e58
waveofmymind <sjun0913@gmail.com>
2023-03-20 08:01:25 +0900

    [FEAT] 코틀린 리팩토링 시작

---
+++ basic1-k/src/main/kotlin/com/example/basic1k/Basic1KApplication.kt
@@ -0,0 +1,11 @@
+package com.example.basic1k
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Basic1KApplication
+
+fun main(args: Array<String>) {
+    runApplication<Basic1KApplication>(*args)
+}
---
+++ basic1-k/src/test/kotlin/com/example/basic1k/Basic1KApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.basic1k
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Basic1KApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
